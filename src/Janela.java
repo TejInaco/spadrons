14733948bb468b094b561ca3af7130fa98748328
Ricardo MG Silva <up201607780@fe.up.pt>
2019-01-25 18:14:56 +0000

    Enviroment SetUp

---
+++ src/Janela.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Janela {
+    private JPanel Painel;
+    private JButton button1;
+    private JButton OKButton;
+    private JTextPane textPane1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
